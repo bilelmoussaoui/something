3e8dbf032a528c1436973d6762318940a0c85c10
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-07-22 07:37:51 +0200

    footer: remove unused links

--- frontend/src/components/layout/Footer.jsx
+++ frontend/src/components/layout/Footer.jsx
@@ -1,29 +1,13 @@
 import React from 'react'
 import { Footer as MFooter } from 'react-materialize'
 function Footer() {
-    const FooterLinks = <ul>
-        <li>
-            <a className="grey-text text-lighten-3" href="#!">Link 1</a>
-        </li>
-        <li>
-            <a className="grey-text text-lighten-3" href="#!">Link 2</a>
-        </li>
-        <li>
-            <a className="grey-text text-lighten-3" href="#!">Link 3</a>
-        </li>
-        <li>
-            <a className="grey-text text-lighten-3" href="#!">Link 4</a>
-        </li>
-    </ul>
 
 
     return <MFooter
         className="black"
         id="footer"
         copyrights="Powered by Linux Store Frontend v0.7.5 a8524a1"
-        links={FooterLinks}
     > <ul>
-
             <li>
                 <a className="grey-text text-lighten-3" href="#!">Popular</a>
             </li>
